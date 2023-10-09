a9e040a39ddf78763a903f3721a68ecf49eecd07
Grotawred <grishavlasko200@gmail.com>
2023-10-09 21:46:54 +0300

    4 homework + lection exersises

---
+++ test.py
@@ -0,0 +1,71 @@
+# text = "@Test"
+# text = text.replace("@", "")
+# text = text[::-1]
+
+# print(text)
+ 
+# word = "Anna"
+# new_word = ""
+# word = word.lower()
+# for i in range(len(word)):
+
+#     new_word += str(word[-(i+1)])
+
+# print(new_word)
+# print(word==new_word)
+
+# while text.find("@")!= -1:
+#     first_index = text.find("@")
+#     last_index = text[first_index:].find(" ")
+#     word = text[first_index:last_index+first_index]
+#     text = text[first_index+1:]
+#     print(word)
+
+
+# words = text.split(" ")
+
+# firstIndex = text.find("@")
+# word = text[firstIndex:]
+
+# lastIndex = word.find[" "]
+
+
+# word = word[0:lastIndex]
+
+# lowerWord = word.lower()
+# capsWord = word.upper()
+# firstBigWord = word.title()
+
+
+# print(text[firstIndex:lastIndex])
+
+
+
+# for word in words:
+#     if word[0] == "@":
+#         print (word)
+
+
+
+
+
+
+
+# numbers = [1,5,7,8,9]
+
+# def min(array):
+#     min = 1000
+#     for number in array:
+#         if(min>number):
+#             min = number
+#     return min
+
+# def max(array):
+#     max = 0
+#     for number in array:
+#         if(max<number):
+#             max = number
+#     return max
+
+# print(min(numbers))
+# print(max(numbers))
